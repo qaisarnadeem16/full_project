5d91f687d8ea7198f420c190f9f1e85d08e6b878
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2022-09-03 10:21:31 +0500

    day2

--- Mobile Shopping Site/index.js
+++ Mobile Shopping Site/index.js
@@ -1,8 +1,30 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
+    var $grid=$('.grid').isotope({
+        // options
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+      });
 
-//     $("#banner-section .owl-carousel").owlCarousel({
-//         dots:true,
-//         items:1
-//     })
-// })
+$(".button-group").on("click","button",function(){
+    var filterValue=$(this).attr('data-filter')
+    $grid.isotope({filter:filterValue});
+})
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
